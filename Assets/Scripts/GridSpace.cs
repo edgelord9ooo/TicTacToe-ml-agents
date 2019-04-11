b2aa35c58a76841467ad7ab6c48d0386bd31fb46
Avery <avery_tan@hotmail.com>
2019-04-10 22:17:59 -0600

    first commit. Complete game with some bugs and refactoring needed

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    private GameController gameController;
+    
+    public void setSpace () 
+    {
+        
+        buttonText.text = gameController.GetPlayerSide();
+        if (String.IsNullOrEmpty(buttonText.text)) {
+            return;
+        }
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    public void SetGameControllerReference (GameController controller)
+    {
+        gameController = controller;
+    }
+}
